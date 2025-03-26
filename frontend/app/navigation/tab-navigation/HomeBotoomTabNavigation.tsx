a39c8604a6f0774773c8b1db13aa4e966064529c
msafryx <mzafry2002@gmail.com>
2025-03-26 17:17:48 +0530

    updated bottom tab navigation

--- frontend/app/navigation/tab-navigation/HomeBotoomTabNavigation.tsx
+++ frontend/app/navigation/tab-navigation/HomeBotoomTabNavigation.tsx
@@ -4,12 +4,30 @@ import HomeProductScreen from "@/components/ui/screen/home/HomeProductScreen";
 import HomeBookmarkScreen from "@/components/ui/screen/home/HomeBookmarkScreen";
 import HomePageScreen from "@/components/ui/screen/home/HomePageScreen";
 import HomeCartScreen from "@/components/ui/screen/home/HomeCartScreen";
+import {Ionicons} from "@expo/vector-icons";
+import {COLORS} from "@/constants/ColourPallete";
 
 const Tab = createBottomTabNavigator();
 export default function HomeBotoomTabNavigation({navigation}:any){
 
     return(
-        <Tab.Navigator>
+        <Tab.Navigator
+        initialRouteName={'Home'}
+        screenOptions={({route, focused}:any)=>({
+            tabBarIcon:({color,size})=>{
+                let iconName;
+                if(route.name==='Menu') iconName = focused? 'menu': 'menu-outline';
+                else if(route.name==='Cart')iconName = focused? 'cart': 'cart-outline';
+                else if(route.name==='Home')iconName = focused? 'home': 'home-outline';
+                else if(route.name==='Bookmark')iconName = focused? 'heart': 'heart-outline';
+                else if(route.name==='Products')iconName = focused? 'grid': 'grid-outline';
+                //@ts-ignore
+                return<Ionicons name={iconName} size={22} color={color}/>
+            },
+            tabBarActiveTintColor:COLORS.darkOrange,
+            tabBarInactiveTintColor:COLORS.darkGray,
+
+        })}>
             <Tab.Screen name={'Menu'} component={HomeMenuScreen}/>
             <Tab.Screen name={'Cart'} component={HomeCartScreen}/>
             <Tab.Screen name={'Home'} component={HomePageScreen}/>
