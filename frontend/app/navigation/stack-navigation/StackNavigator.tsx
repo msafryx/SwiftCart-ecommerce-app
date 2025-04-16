f34325e3000294e41eb0a075c6aeb3071332f541
msafryx <mzafry2002@gmail.com>
2025-04-16 23:07:29 +0530

    added the complete navigations from menu

--- frontend/app/navigation/stack-navigation/StackNavigator.tsx
+++ frontend/app/navigation/stack-navigation/StackNavigator.tsx
@@ -1,5 +1,14 @@
 import {createStackNavigator} from '@react-navigation/stack'
 import HomeBottomTabNavigation from "@/app/navigation/tab-navigation/HomeBotoomTabNavigation";
+import CustomerCareScreen from "@/components/ui/screen/other/CustomerCareScreen";
+import DailyDealsScreen from "@/components/ui/screen/other/DailyDealsScreen";
+import LatestProductsScreen from "@/components/ui/screen/other/LatestProductsScreen";
+import MyOrderScreen from "@/components/ui/screen/other/MyOrderScreen";
+import NotificationsScreen from "@/components/ui/screen/other/NotificationsScreen";
+import ProfileScreen from "@/components/ui/screen/other/ProfileScreen";
+import SettingsScreen from "@/components/ui/screen/other/SettingsScreen";
+import VouchersScreen from "@/components/ui/screen/other/VouchersScreen";
+import LogOutScreen from "@/components/ui/screen/other/LogOutScreen";
 const Stack = createStackNavigator();
 export default function StackNavigator(){
     return(
@@ -7,6 +16,34 @@ export default function StackNavigator(){
             <Stack.Screen name={'Process'}
                           options={{headerLeft:()=>null, headerShown:false}}
                           component={HomeBottomTabNavigation}/>
+            <Stack.Screen name={'CustomerCare'}
+                          options={{title:'Customer Care'}}
+                          component={CustomerCareScreen}/>
+            <Stack.Screen name={'DailyDeals'}
+                          options={{title:'Daily Deals'}}
+                          component={DailyDealsScreen}/>
+            <Stack.Screen name={'LatestProducts'}
+                          options={{title:'Latest Products'}}
+                          component={LatestProductsScreen}/>
+            <Stack.Screen name={'MyOrders'}
+                          options={{title:'My Orders'}}
+                          component={MyOrderScreen}/>
+            <Stack.Screen name={'Notifications'}
+                          options={{title:'Notifications'}}
+                          component={NotificationsScreen}/>
+            <Stack.Screen name={'Profile'}
+                          options={{title:'Profile'}}
+                          component={ProfileScreen}/>
+            <Stack.Screen name={'Settings'}
+                          options={{title:'Settings'}}
+                          component={SettingsScreen}/>
+            <Stack.Screen name={'Vouchers'}
+                          options={{title:'Vouchers'}}
+                          component={VouchersScreen}/>
+            <Stack.Screen name={'LogOut'}
+                          options={{title:'Log Out'}}
+                          component={LogOutScreen}/>
+
         </Stack.Navigator>
     )
 }
--- frontend/components/ui/screen/home/HomeMenuScreen.tsx
+++ frontend/components/ui/screen/home/HomeMenuScreen.tsx
@@ -1,9 +1,21 @@
 import {View, Text, StyleSheet} from "react-native";
+import * as React from 'react';
 
-export default function HomeMenuScreen(){
+import { Menu } from 'react-native-paper';
+import {COLORS} from "@/constants/ColourPallete";
+
+export default function HomeMenuScreen({navigation}:any){
     return(
         <View style={styles.container}>
-            <Text>Home Menu Screen</Text>
+                <Menu.Item leadingIcon="redo" onPress={() => {navigation.navigate('MyOrders')}} title="My Orders" />
+                <Menu.Item leadingIcon="undo" onPress={() => {navigation.navigate('Settings')}} title="Settings" />
+                <Menu.Item leadingIcon="content-copy" onPress={() => {navigation.navigate('DailyDeals')}} title="Daily Deals"  />
+                <Menu.Item leadingIcon="content-paste" onPress={() => {navigation.navigate('Vouchers')}} title="Vouchers" />
+                <Menu.Item leadingIcon="content-cut" onPress={() => {navigation.navigate('Notifications')}} title="Notifications"  />
+                <Menu.Item leadingIcon="content-cut" onPress={() => {navigation.navigate('LatestProducts')}} title="Latest Products"  />
+                <Menu.Item leadingIcon="content-cut" onPress={() => {navigation.navigate('Profile')}} title="Profile"  />
+                <Menu.Item leadingIcon="content-cut" onPress={() => {navigation.navigate('CustomerCare')}} title="Customer Care"  />
+                <Menu.Item leadingIcon="content-cut" onPress={() => {navigation.navigate('LogOut')}} title="Logout"  />
         </View>
     )
 }
@@ -11,7 +23,6 @@ export default function HomeMenuScreen(){
 const styles = StyleSheet.create({
     container:{
         flex:1,
-        alignItems:'center',
-        justifyContent:'center'
+        backgroundColor:COLORS.light
     }
 })
