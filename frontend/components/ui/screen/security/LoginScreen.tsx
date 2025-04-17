e2fa74c8714af824c925877dbdda9b244ea67cae
msafryx <mzafry2002@gmail.com>
2025-04-17 14:46:22 +0530

    completed the login page

--- frontend/components/ui/screen/security/LoginScreen.tsx
+++ frontend/components/ui/screen/security/LoginScreen.tsx
@@ -1,12 +1,16 @@
-import {View, Text, StyleSheet, ScrollView, Image} from "react-native";
+import {View, Text, StyleSheet, ScrollView, Image, TouchableOpacity} from "react-native";
 import {COLORS} from "@/constants/ColourPallete";
 import {TextInput} from "react-native-paper";
 import {useState} from "react";
+import {Icon} from "react-native-paper";
 
 const logo = require('../../../../assets/images/logo/logo-sc.png');
 export default function LoginScreen(){
     const [email, setEmail]= useState('');
     const [password, setPassword]= useState('');
+    const [passwordDisplayState, setPasswordDisplayState]= useState(false);
+
+
     return(
         <ScrollView style={styles.container}>
            <View style={styles.logoWrapper}>
@@ -16,6 +20,7 @@ export default function LoginScreen(){
                 <View style={styles.formGroup}>
                     <TextInput
                         label="Root Email"
+                        mode={'outlined'}
                         value={email}
                         onChangeText={text => setEmail(text)}
                     />
@@ -24,16 +29,84 @@ export default function LoginScreen(){
                 <View style={styles.formGroup}>
                     <TextInput
                         label="Password"
+                        mode={'outlined'}
+                        secureTextEntry={!passwordDisplayState}
                         value={password}
                         onChangeText={text => setPassword(text)}
+                        right={<TextInput.Icon onPress={()=>{
+                            setPasswordDisplayState(!passwordDisplayState)
+                        }} size={20} icon={passwordDisplayState?'eye':'eye-off'}/> }
                     />
                 </View>
+                <TouchableOpacity style={styles.forgetPasswordButton}>
+                    <Text style={styles.forgetPasswordText}> Forgot Password</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.loginButton}>
+                    <Text style={styles.loginText}> Login</Text>
+                </TouchableOpacity>
+                <Text style={styles.separateText}> OR </Text>
+                <View style={styles.socialLoginWrapper}>
+                    <TouchableOpacity style={styles.iconOuter}>
+                        <Icon size={20} source={'google'}></Icon>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.iconOuter}>
+                        <Icon size={20} source={'facebook'}></Icon>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.iconOuter}>
+                        <Icon size={20} source={'twitter'}></Icon>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.iconOuter}>
+                        <Icon size={20} source={'github'}></Icon>
+                    </TouchableOpacity>
+                </View>
+                <TouchableOpacity style={{...styles.loginButton,backgroundColor:COLORS.primary}}>
+                    <Text style={styles.loginText}> Register with the email</Text>
+                </TouchableOpacity>
+
+
             </View>
         </ScrollView>
     )
 }
 
 const styles = StyleSheet.create({
+    iconOuter:{
+        backgroundColor:COLORS.darkGray,
+        width:50,
+        height:50,
+        borderRadius:50,
+        alignItems:'center',
+        justifyContent:'center'
+    },
+    socialLoginWrapper:{
+        flexDirection:'row',
+        marginTop:20,
+        marginBottom:10,
+        justifyContent:'space-around'
+
+    },
+    separateText:{
+        textAlign:'center',
+        marginTop:30
+    },
+    loginText:{
+        color:COLORS.light,
+    },
+    loginButton:{
+        backgroundColor:COLORS.blue,
+        height:50,
+        marginTop:30,
+        borderRadius:3,
+        alignItems:'center',
+        justifyContent:"center"
+    },
+    forgetPasswordText:{
+        color:COLORS.blue,
+        textDecorationLine:"underline"
+    },
+    forgetPasswordButton:{
+        alignItems:'flex-end'
+    },
     formGroup:{
         marginBottom:10
     },
--- frontend/constants/ColourPallete.tsx
+++ frontend/constants/ColourPallete.tsx
@@ -3,7 +3,9 @@ import type from "ajv/lib/vocabularies/jtd/type";
 export const COLORS={
     darkGray:'#989fa1',
     darkOrange:'#cc2c1e',
-    light:'#FFF'
+    light:'#FFF',
+    blue:'#3498db',
+    primary:'#2c3e50',
 }as const;
 
 export type ColorTypes = keyof typeof COLORS;
