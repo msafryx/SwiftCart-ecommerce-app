f639ada181632621a86cc4c08ada0fbc62737dfb
msafryx <mzafry2002@gmail.com>
2025-05-26 10:15:01 +0530

    completed bookmark screen

--- frontend/SwiftCart/components/ui/screen/home/HomeBookmarkScreen.tsx
+++ frontend/SwiftCart/components/ui/screen/home/HomeBookmarkScreen.tsx
@@ -1,19 +1,49 @@
-import {View, Text, StyleSheet} from "react-native";
+import {View, Text, StyleSheet, ScrollView} from "react-native";
+import {Searchbar} from "react-native-paper";
+import {useState} from "react";
+import DisplayTypeWidget from "@/components/ui/screen/share/DisplayTypeWidget";
 import {COLORS} from "@/constants/ColourPallete";
+import ProductGridViewWidget from "@/components/ui/screen/home/widget/ProductGridViewWidget";
+import ProductListViewWidget from "@/components/ui/screen/home/widget/ProductListViewWidget";
 
-export default function HomeBookmarkScreen(){
+export default function HomeBookMarkScreen(){
+    const [searchQuery, setSearchQuery] = useState('');
+    const [isGridEnabled, setIsGridEnabled] = useState(true);
+
     return(
         <View style={styles.container}>
-            <Text>Home Bookmark Screen</Text>
+            <Searchbar
+                placeholder="Search"
+                onChangeText={setSearchQuery}
+                value={searchQuery}
+            />
+            <DisplayTypeWidget
+                callBack ={(state:boolean)=>(state)}/>
+
+            {isGridEnabled?(
+                <ScrollView>
+                    <ProductGridViewWidget/>
+                    <ProductGridViewWidget/>
+                    <ProductGridViewWidget/>
+                    <ProductGridViewWidget/>
+                </ScrollView>
+            ):(
+                <ScrollView>
+                    <ProductListViewWidget/>
+                    <ProductListViewWidget/>
+                    <ProductListViewWidget/>
+                    <ProductListViewWidget/>
+                </ScrollView>
+            )}
+
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container:{
-        flex:1,
         backgroundColor:COLORS.light,
-        alignItems:'center',
-        justifyContent:'center'
+        flex:1,
+        padding:10
     }
 })
