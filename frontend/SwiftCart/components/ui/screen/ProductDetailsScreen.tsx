e381f859a058671385c95e15ba8ca676e55877fb
msafryx <mzafry2002@gmail.com>
2025-05-27 14:46:14 +0530

    added buy now, add to cart, review and recommended buttons

--- frontend/SwiftCart/components/ui/screen/ProductDetailsScreen.tsx
+++ frontend/SwiftCart/components/ui/screen/ProductDetailsScreen.tsx
@@ -1,12 +1,8 @@
-import {View, Text, StyleSheet, ScrollView, Image, TouchableOpacity} from "react-native";
-import {Searchbar} from "react-native-paper";
+import {View, Text, StyleSheet, Image, TouchableOpacity} from "react-native";
 import {useState} from "react";
-import DisplayTypeWidget from "@/components/ui/screen/share/DisplayTypeWidget";
 import {COLORS} from "@/constants/ColourPallete";
-import ProductGridViewWidget from "@/components/ui/screen/home/widget/ProductGridViewWidget";
-import ProductListViewWidget from "@/components/ui/screen/home/widget/ProductListViewWidget";
-import CartListViewWidget from "@/components/ui/screen/home/widget/CartListViewWidget";
-import CartGridViewWidget from "@/components/ui/screen/home/widget/CartGridViewWidget";
+import {Icon} from "react-native-paper";
+
 
 export default function ProductDetailsScreen(){
     const productImages=[
@@ -14,7 +10,9 @@ export default function ProductDetailsScreen(){
         {uri:'https://media.ldlc.com/ld/products/00/05/88/62/LD0005886202_1.jpg'},
         {uri:'https://th.bing.com/th/id/R.929e4905e29484905b2befbf9ae080df?rik=bk%2b3rzmiGcgHQw&pid=ImgRaw&r=0'}
     ]
-    const [primaryImage , setPrimaryImage] = useState(productImages[0].uri)
+    const [primaryImage , setPrimaryImage] = useState(productImages[0].uri);
+    const [status , setStatus] = useState(true);
+
     return(
         <View style={styles.container}>
             <View style={styles.top}>
@@ -34,21 +32,64 @@ export default function ProductDetailsScreen(){
 
                 </View>
             </View>
-            <View >
-                <Text style={{fontWeight:'bold', fontSize:20, marginTop:5}}>Apple iPhone 13 128GB | MySoftlogic.lk</Text>
-                <Text>iPhone 13 5G smartphone offers Super Retina XDR OLED display with Dolby Vision HDR and IP68 rated water or dust resistant protection.</Text>
+            <View style={{}}>
+                <Text style={{fontWeight:'bold', fontSize:20, marginTop:15}}>Apple iPhone 13 128GB | MySoftlogic.lk</Text>
+                <Text style={{marginTop:10}}>iPhone 13 5G smartphone offers Super Retina XDR OLED display with Dolby Vision HDR and IP68 rated water or dust resistant protection.</Text>
 
                 <View style={{marginTop:10, flexDirection:"row"}}>
-                    <Text style={{color:COLORS.darkOrange,fontWeight:'bold',fontSize:15, marginRight:10 }}> 2500 UAD</Text>
-                    <Text style={{color:COLORS.darkOrange,fontWeight:'bold',fontSize:15,textDecorationLine:'line-through' }}> 2900 UAD</Text>
+                    <Text style={{color:COLORS.darkOrange,fontWeight:'bold',fontSize:15, marginRight:10 }}>2500 UAD</Text>
+                    <Text style={{color:COLORS.darkOrange,fontWeight:'bold',fontSize:15,textDecorationLine:'line-through' }}>2900 UAD</Text>
 
                 </View>
+            </View>
+            <View style={{marginTop:20, flexDirection:"row",justifyContent:'flex-end'}}>
+                <TouchableOpacity style={{...styles.button, backgroundColor:COLORS.primary, marginRight:10}}>
+                    <Icon size={20} source={'cart'} color={COLORS.light}/>
+                </TouchableOpacity>
+                <TouchableOpacity style={{...styles.button, backgroundColor:COLORS.blue}}>
+                    <Text style={{color:COLORS.light}}>Buy Now</Text>
+                </TouchableOpacity>
+            </View>
+            <View style={styles.tab}>
+                <TouchableOpacity
+                    onPress={()=>setStatus(true)}
+                    style={{...styles.tabButton, backgroundColor:status?COLORS.primary:COLORS.light}}>
+                    <Text style={{color:status?COLORS.light:COLORS.primary}}>Reviews</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    onPress={()=>setStatus(false)}
+                    style={{...styles.tabButton, backgroundColor:status?COLORS.light:COLORS.primary}}>
+                    <Text style={{color:status?COLORS.primary:COLORS.light}}>Recommended</Text>
+                </TouchableOpacity>
+
             </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
+    tabButton:{
+        justifyContent:'center',
+        alignItems:'center',
+        height:35,
+        width:120
+    },
+    tab:{
+        width:'100%',
+        borderBottomWidth:1,
+        borderBottomColor:COLORS.primary,
+        marginBottom:20,
+        marginTop:20,
+        flexDirection:'row'
+    },
+    button:{
+        width:120,
+        height:35,
+        borderRadius:50,
+        alignItems:"center",
+        justifyContent:"center"
+
+    },
     displayImage:{
         width:'100%',
         height:'100%'
